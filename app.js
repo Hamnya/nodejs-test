9b77aa1700ecde5399716d9d28f6c57039437dfa
Hamnya <ihamnya@gmail.com>
2019-01-21 18:03:57 +0900

    addd

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ steem.api.setOptions({url: 'https://api.steemit.com'});
 steem.api.getAccounts(['ned','dan'], function(err, response){
   console.log(err, response);
 });
+
+console.log("Good bye~ :)");
