f3e1b47c8c3a52fc55e19f697bbd241fe89aa05d
Hamnya <ihamnya@gmail.com>
2019-01-22 16:50:35 +0900

    fix wrong spell

--- winstonLog_test.js
+++ winstonLog_test.js
@@ -1,5 +1,5 @@
 var winston = require('winston'); //로그 처리 모듈
-var winstonDaily = request('winston-daily-rotate-file'); // 로그 일별 처리 모듈
+var winstonDaily = require('winston-daily-rotate-file'); // 로그 일별 처리 모듈
 var moment = require('moment'); //시간 처리 모듈
 
 function timeStampFormat(){
